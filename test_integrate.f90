640a105fa608447255417c565649d8eb56d202d0
rafeh-rehan <105065877+rafeh-rehan@users.noreply.github.com>
2022-05-06 12:32:38 -0400

    Update test_integrate.f90

--- test_integrate.f90
+++ test_integrate.f90
@@ -5,7 +5,8 @@ use integrate
 
 implicit double precision (a-h,o-z)
 
-n = 450 ! take Big_O( (b-a)/2 * 10 )
+! Number of intervals (multiple of 3)
+n = 450 
 
 
 do i = 1, 10**4
@@ -15,19 +16,14 @@ call random_number(dd)
 b = 10.0d0**5*dd
 
 call simp(f,a,b,n,y)
-call gauss(f,a,b,n,f_integral)
 z = f_int(a,b)
 
 
 error = abs(y - z)/abs(z)
 e_max = max(error,e_max)
 
-gerror = abs(f_integral - z)/abs(z)
-ge_max = max(gerror,ge_max)
-
 end do
 print*,'Max relative error simp:', e_max
-print*,'Max relative error gauss:', ge_max
 
 
 
